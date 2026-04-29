8b710dafb680a5b24d34834454658a74cca2841d
AT3721 <anthanh3007@gmail.com>
2026-04-29 11:35:19 +0700

    add

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
 
-function Hero(image, top, left, size){
+
+function Hero(image, top, left, size, speed) {
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
+  this.speed = speed;
 
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+  this.getHeroElement = function () {
+    return '<img width="' + this.size + '"' +
+      ' height="' + this.size + '"' +
+      ' src="' + this.image + '"' +
+      ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
   }
 
-  this.moveRight = function(){
-    this.left += 20;
+  this.moveRight = function () {
+    this.left += this.speed;
     console.log('ok: ' + this.left);
   }
-
+  this.moveLeft = function () {
+    this.left -= this.speed;
+  }
+  this.moveUp = function () {
+    this.top -= this.speed;
+  }
+  this.moveDown = function () {
+    this.top += this.speed;
+  }
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+var hero = new Hero('doraemon.png', 20, 30, 200, 6);
 
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
+function start() {
+  let maxWidth = window.innerWidth - hero.size;
+  let maxHeight = window.innerHeight - hero.size;
+
+  if (hero.left < maxWidth && hero.top <= 20) {
     hero.moveRight();
+  } else if (hero.left >= maxWidth && hero.top < maxHeight) {
+    hero.moveDown();
+  } else if (hero.top >= maxHeight && hero.left > 30) {
+    hero.moveLeft();
+  } else if (hero.left <= 30 && hero.top > 20) {
+    hero.moveUp();
   }
+
   document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+  setTimeout(start, 20)
 }
 
 start();
